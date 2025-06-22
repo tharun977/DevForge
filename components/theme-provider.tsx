d1d7f0cd36bb305b6023a1f401c88143b61c53ac
Tharun <tharunraman10@gmail.com>
2025-06-22 17:51:27 +0530

    theme toggle  bug fix

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -1,11 +1,28 @@
 'use client'
 
 import * as React from 'react'
-import {
-  ThemeProvider as NextThemesProvider,
-  type ThemeProviderProps,
-} from 'next-themes'
+import { ThemeProvider as NextThemesProvider } from 'next-themes'
 
-export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>
+interface Props {
+  children: React.ReactNode
+  attribute?: 'class' | 'data-theme'
+  defaultTheme?: string
+  enableSystem?: boolean
+}
+
+export function ThemeProvider({
+  children,
+  attribute = 'class',
+  defaultTheme = 'system',
+  enableSystem = true,
+}: Props) {
+  return (
+    <NextThemesProvider
+      attribute={attribute}
+      defaultTheme={defaultTheme}
+      enableSystem={enableSystem}
+    >
+      {children}
+    </NextThemesProvider>
+  )
 }
